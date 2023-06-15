d3e8ccfceaa9a6cd7a66993f0a1c26714f9b7343
emilioff8822 <emilioff@hotmail.it>
2023-06-15 15:54:52 +0200

    commit-dashboardcontroller

--- login1/resources/views/guest/home.blade.php
+++ login1/resources/views/guest/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.guest')
 
 @section('content')
-    <div class="container">
+    <div class="container text-center my-5 ">
         <h1>Home Pubblica</h1>
         <p>Qui verra innestato VUEJS</p>
 
