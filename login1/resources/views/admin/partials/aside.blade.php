19d7e81612f1100b96811a22bde77e81f72206b3
emilioff8822 <emilioff@hotmail.it>
2023-06-16 21:18:21 +0200

    commit-paginator

--- login1/resources/views/admin/partials/aside.blade.php
+++ login1/resources/views/admin/partials/aside.blade.php
@@ -2,7 +2,7 @@
     <nav>
         <ul>
             <li><a href="{{ route('admin.home') }}"><i class="fa-solid fa-chart-line"></i> Dashboard</a></li>
-            <li><a href="#"><i class="fa-solid fa-list"></i> Posts</a></li>
+            <li><a href="{{ route('admin.posts.index') }}"><i class="fa-solid fa-list"></i> Posts</a></li>
             <li><a href="#"><i class="fa-solid fa-folder-plus"></i> New Post</a></li>
         </ul>
     </nav>
---
+++ login1/resources/views/admin/posts/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container p-5">
+        <h2 class="fs-4 text-secondary my-4">
+            Elenco Post
+        </h2>
+
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">#ID</th>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Data</th>
+                    <th scope="col">Azioni</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                    <tr>
+                        <td>{{ $post->id }}</td>
+                        <td>{{ $post->title }}</td>
+                        @php
+                            $date = date_create($post->date);
+                        @endphp
+                        <td>{{ date_format($date, 'd/m/Y') }}</td>
+                        <td><a href="" class="btn btn-primary">VAI</a></td>
+                    </tr>
+                @endforeach
+
+
+
+
+            </tbody>
+        </table>
+        <div>
+            {{ $posts->links() }}
+        </div>
+
+    </div>
+@endsection
